5c56f4bc6974cb715dbb8b2e9c206b38e512a6e6
Oleg Gelfand <oleggelfand.1@gmail.com>
2020-08-09 20:58:45 -0400

    publishing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState, useEffect } from "react";
+import "./App.css";
+import Main from "./Main";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [projectIndex, setProjectIndex] = useState("");
+  console.log("index from app", projectIndex);
+  useEffect(() => {
+    const makeAPICall = async () => {
+      const projectURL =
+        "https://spreadsheets.google.com/feeds/list/1ZgSrmkNKNaoR9jbnrp_he1qtHLmGJsq191HpCkT86sM/od6/public/values?alt=json";
+      const resp = await fetch(`${projectURL}`);
+      let json = await resp.json();
+      setProjects(json);
+    };
+    makeAPICall();
+  }, []);
+  if (projects.feed) {
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <UniversalContext.Provider
+        value={{
+          //anything i need for application
+          projects,
+          projectIndex,
+          setProjectIndex,
+        }}
+      >
+        <Main />
+      </UniversalContext.Provider>
     </div>
   );
 }
 
 export default App;
+export const UniversalContext = createContext();
+
